b851d5b1977da8652c2f0762258ea1cb90d3b640
Zayon <pablo.godinez@zayon.eu>
2014-10-09 14:40:52 +0200

    create route, action and view of the dashboard

--- src/Ema/RgementBundle/Controller/DefaultController.php
+++ src/Ema/RgementBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('EmaRgementBundle:Default:index.html.twig');
     }
+
+    public function dashboardAction()
+    {
+        return $this->render('EmaRgementBundle:Default:dashboard.html.twig');
+    }
 }
