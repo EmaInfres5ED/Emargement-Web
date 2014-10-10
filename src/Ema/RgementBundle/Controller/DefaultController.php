f5914bab20c62b3b3501ad1043fa3782c1c22d3c
Pafounet <travianpaf@gmail.com>
2014-10-10 09:59:26 +0200

    Update routes + noms

--- src/Ema/RgementBundle/Controller/DefaultController.php
+++ src/Ema/RgementBundle/Controller/DefaultController.php
@@ -16,18 +16,18 @@ class DefaultController extends Controller
     }
     public function statsfrequencyDelayAction()
     {
-        return $this->render('EmaRgementBundle:Default:stats/frequency/Delay.html.twig');
+        return $this->render('EmaRgementBundle:Default:stats/frequency/delay.html.twig');
     }
     public function statsfrequencyAbsenceAction()
     {
-        return $this->render('EmaRgementBundle:Default:stats/frequency/Absence.html.twig');
+        return $this->render('EmaRgementBundle:Default:stats/frequency/absence.html.twig');
     }
     public function statsaccumulationDelayAction()
     {
-        return $this->render('EmaRgementBundle:Default:stats/accumulation/Delay.html.twig');
+        return $this->render('EmaRgementBundle:Default:stats/accumulation/delay.html.twig');
     }
     public function statsaccumulationAbsenceAction()
     {
-        return $this->render('EmaRgementBundle:Default:stats/accumulation/Absence.html.twig');
+        return $this->render('EmaRgementBundle:Default:stats/accumulation/absence.html.twig');
     }
 }
