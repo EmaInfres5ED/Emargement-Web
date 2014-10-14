5cfb7c89d557ef08594449f486dc0ed1dc68f793
anto1nee <gely.antoine@gmail.com>
2014-10-14 14:52:44 +0200

    Add creat JSON promos/cours et web services

--- src/Ema/RgementBundle/Controller/CronController.php
+++ src/Ema/RgementBundle/Controller/CronController.php
@@ -18,7 +18,6 @@ class CronController extends Controller
 		$this->updateEtudiants();
 		
 		return new Response('Update OK');
-	
 	}
 
 	//URL: http://localhost/Emargement-Web/web/app_dev.php/cron/update/promotions
@@ -36,8 +35,96 @@ class CronController extends Controller
 		return new Response('Update Etudiants OK');
 	}
 	
+	//URL: http://localhost/Emargement-Web/web/app_dev.php/cron/update/first
+	public function updateFirstJsonAction()
+	{
+		$this->updateFirstJson();
+		return new Response('Update OK');
+	}
 	
-
+	public function updateFirstJson()
+	{
+		$filename = "first.json";
+		$file = fopen($filename, 'w+');
+		fputs($file, $this->getJsonCoursAndPromos());
+		fclose($file);
+	}
+	
+	private function getJsonCoursAndPromos()
+	{
+		$response = array();
+		$promotions = $this->getPromotions();
+		
+		foreach ($promotions as $promo) {
+			$one = array();
+			$one["name"] = $promo->getLibelle();
+			$one["id"] = $promo->getId();
+			
+			$listCours = array();
+			
+			$cours = $this->getCoursByPromo($promo->getIdCybema());
+			foreach ($cours as $cour) {
+				
+				if(isset($cour[13]) && !empty($cour[13]))
+					$c['date'] = $cour[13];
+				if(isset($cour[17]) && !empty($cour[17]))
+					$c['hd'] = $cour[17];
+				if(isset($cour[19]) && !empty($cour[19]))
+					$c['hf'] = $cour[19];
+				if(isset($cour[27]) && !empty($cour[27]))
+					$c['name'] = $cour[27];
+				if(isset($cour[29]) && !empty($cour[29]))
+					$c['salle'] = $cour[29];
+				if(isset($cour[33]) && !empty($cour[33]))
+					$c['prof'] = $cour[33];
+				if(isset($cour[35]) && !empty($cour[35]))
+					$c['groupe'] = $cour[35];
+				
+				array_push($listCours, $c);
+			}
+		
+			$one["cours"] = $listCours;
+			
+			array_push($response, $one);
+		}
+		
+		return json_encode($response);
+	}
+	
+	private function getPromotions()
+	{
+		$em = $this->getDoctrine()->getManager();
+        $repoPromo = $em->getRepository('EmaRgementBundle:Promotion');
+		return $repoPromo->findAll();	
+	}
+	
+	
+	private function getCoursByPromo($idPromoCybema)
+	{	
+		$today = $this->getToday();
+		$http = "http://webdfd.mines-ales.fr/cybema/cgi-bin/cgiempt.exe?TYPE=planning_txt&DATEDEBUT=".$today."&DATEFIN=".$today."&TYPECLE=p0cleunik&VALCLE=".$idPromoCybema;
+		$csv = file_get_contents($http);
+		return $this->csvToJson($csv);
+	}
+	
+	
+	private function getToday()
+	{	
+		$date = getdate();
+		
+		$year = $date['year'];
+		$month = $date['mon'];
+		$day = $date['mday'];
+		
+		if($month >= 1 && $month <= 9)
+			$month = "0".$month;
+		
+		if($day >= 1 && $day <= 9)
+			$day = "0".$day;
+			
+		return $year . $month . $day;		
+	}
+	
 	private function updatePromotions()
 	{
 		//Promotion Repository
--- src/Ema/RgementBundle/Controller/TabletteController.php
+++ src/Ema/RgementBundle/Controller/TabletteController.php
@@ -3,16 +3,41 @@
 namespace Ema\RgementBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Ema\RgementBundle\Entity\Promotion;
 
 class TabletteController extends Controller
 {
 
-	/*public getPromos()
-	{
+	//URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first
+	public function firstAction()
+	{		
+		$filename = "first.json";
+		$file = fopen($filename, 'r');
+		$json = fgets($file);
+		fclose($file);
+		
+		return new Response($json);
+	}
+
+	//URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first/force
+	public function firstForceAction()
+	{		
 	
-	}*/
+	
+	
+		//Réaliser l'update de CronController:updateFirstJson()
 	
 	
 	
 	
+	
+		$filename = "first.json";
+		$file = fopen($filename, 'r');
+		$json = fgets($file);
+		fclose($file);
+		
+		return new Response($json);
+	}
 }
