e37aca74d57ae93861ea6dc92659d749e88a1cde
anto1nee <gely.antoine@gmail.com>
2014-10-16 13:49:43 +0200

    change tab to spaces

--- src/Ema/RgementBundle/Controller/TabletteController.php
+++ src/Ema/RgementBundle/Controller/TabletteController.php
@@ -10,40 +10,40 @@ use Ema\RgementBundle\Controller\CronController;
 
 class TabletteController extends Controller
 {
-	/**
-	* Web Service Action
-	* Return JSON file with cours by promo
-	* URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first
-	*/
-	public function firstAction()
-	{		
-		//Read Json file
-		$filename = "first.json";
-		$file = fopen($filename, 'r');
-		$json = fgets($file);
-		fclose($file);
-		
-		return new Response($json);
-	}
+    /**
+    * Web Service Action
+    * Return JSON file with cours by promo
+    * URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first
+    */
+    public function firstAction()
+    {       
+        //Read Json file
+        $filename = "first.json";
+        $file = fopen($filename, 'r');
+        $json = fgets($file);
+        fclose($file);
+        
+        return new Response($json);
+    }
 
-	/**
-	* Web Service Action
-	* Update JSON file
-	* Return JSON file with cours by promo
-	* URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first/force
-	*/
-	public function firstForceAction()
-	{		
-		//Update first JSON
-		$cronController = $this->get('cron_controller_service');
-		$cronController->updateFirstJson();
-	
-		//Read Json file
-		$filename = "first.json";
-		$file = fopen($filename, 'r');
-		$json = fgets($file);
-		fclose($file);
-		
-		return new Response($json);
-	}
+    /**
+    * Web Service Action
+    * Update JSON file
+    * Return JSON file with cours by promo
+    * URL: http://localhost/Emargement-Web/web/app_dev.php/tablette/first/force
+    */
+    public function firstForceAction()
+    {       
+        //Update first JSON
+        $cronController = $this->get('cron_controller_service');
+        $cronController->updateFirstJson();
+    
+        //Read Json file
+        $filename = "first.json";
+        $file = fopen($filename, 'r');
+        $json = fgets($file);
+        fclose($file);
+        
+        return new Response($json);
+    }
 }
