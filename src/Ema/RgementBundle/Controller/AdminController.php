fb295fbf2bea8140ec498089f192b71bbceb082e
Zayon <pablo.godinez@zayon.eu>
2014-11-20 16:04:41 +0100

    ajax response mock success message

--- src/Ema/RgementBundle/Controller/AdminController.php
+++ src/Ema/RgementBundle/Controller/AdminController.php
@@ -3,13 +3,19 @@
 namespace Ema\RgementBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Ema\RgementBundle\Entity\Message;
 
 class AdminController extends Controller
 {
     public function ajaxSynchronizeStudentsAndPromosAction()
     {
-        return $this->render('EmaRgementBundle:Admin:ajaxSynchronizeStudentsAndPromos.html.twig', array(
-                // ...
-            ));    }
+        $response = new JsonResponse();
+        $response->setData(array(
+            'type' => Message::TYPE_SUCCESS,
+            'message' => '<insert message HERE>'
+        ));
 
+        return $response;
+    }
 }
