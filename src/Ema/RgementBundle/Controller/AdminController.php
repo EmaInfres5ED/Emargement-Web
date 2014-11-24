85170690eeaa275e486c36baea5d73021dfa05e2
Zayon <pablo.godinez@zayon.eu>
2014-11-24 15:17:52 +0100

    add ajax function to update courses

--- src/Ema/RgementBundle/Controller/AdminController.php
+++ src/Ema/RgementBundle/Controller/AdminController.php
@@ -30,4 +30,24 @@ class AdminController extends Controller
 
         return $response;
     }
+
+    public function ajaxSynchronizeCoursAction()
+    {
+        $response = new JsonResponse();
+
+        $this->cronController = $this->get('cron_controller_service');
+        if ($this->cronController->updateFirstJsonAction()) {
+            $response->setData(array(
+                'type' => Message::TYPE_SUCCESS,
+                'message' => 'Mise à jour des cours effectuée avec success.'
+            ));
+        } else {
+            $response->setData(array(
+                'type' => Message::TYPE_ERROR,
+                'message' => 'Echec de la mise à jour des cours.'
+            ));
+        }
+
+        return $response;
+    }
 }
