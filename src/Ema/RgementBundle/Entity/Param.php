b8fc4c8ac3030766e38220d19989d3314a684776
anto1nee <gely.antoine@gmail.com>
2014-11-24 15:43:48 +0100

    Add login for tablette

---
+++ src/Ema/RgementBundle/Entity/Param.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Ema\RgementBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Param
+ *
+ * @ORM\Table(name="param")
+ * @ORM\Entity
+ */
+class Param
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=100, nullable=false)
+     */
+    private $value;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Param
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return Param
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
