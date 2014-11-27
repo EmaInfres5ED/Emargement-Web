589173e3a9bacc8df9a4a27b8d48541ab67f6cef
Zayon <pablo.godinez@zayon.eu>
2014-11-27 11:44:11 +0100

    updated routes to use the Jusify controller

--- src/Ema/RgementBundle/Controller/JustifyController.php
+++ src/Ema/RgementBundle/Controller/JustifyController.php
@@ -6,4 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class JustifyController extends Controller
 {
+    public function justifyListAction()
+    {
+        return $this->render('EmaRgementBundle:Justify:list.html.twig');
+    }
+
+    public function justifyAbsenceAction()
+    {
+        return $this->render('EmaRgementBundle:Justify:absence.html.twig');
+    }
+
+    public function justifyDelayAction()
+    {
+        return $this->render('EmaRgementBundle:Justify:delay.html.twig');
+    }
 }
