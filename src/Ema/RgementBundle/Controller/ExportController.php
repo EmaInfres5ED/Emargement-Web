e55faa2e2f4288644a189f10988df9e40e69b457
Yoldark34 <yoldark@gmail.com>
2014-12-01 11:35:53 +0100

    Allow to export absences and retard from Export controller

--- src/Ema/RgementBundle/Controller/ExportController.php
+++ src/Ema/RgementBundle/Controller/ExportController.php
@@ -3,12 +3,36 @@
 namespace Ema\RgementBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ExportController extends Controller
 {
+
+    private $excelService;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        parent::setContainer($container);
+        $this->initialize();
+    }
+
+    private function initialize()
+    {
+        $this->excelService = $this->get('excel_service');
+    }
+
     public function listAction()
     {
         return $this->render('EmaRgementBundle:Default:export.html.twig');
+    }
+
+    public function exportAction($studentId, $dateFrom, $dateTo)
+    {
+        $phpExcelObject = $this->excelService->createFromTemplate('export_template');
+
+        $this->excelService->setProperties('EMA', 'EMA', 'Export_test');
+
+        return $this->excelService->export('Export_test');
     }
 
 }
