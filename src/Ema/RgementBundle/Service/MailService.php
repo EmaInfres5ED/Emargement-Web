22841c02ab1ac00cbfd05ebdf8c4ba657c7659c3
Yoldark34 <yoldark@gmail.com>
2014-12-02 10:29:59 +0100

    Add content type

--- src/Ema/RgementBundle/Service/MailService.php
+++ src/Ema/RgementBundle/Service/MailService.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MailService extends Controller
 {
-    public function send($from, $to, $subject, $content)
+    public function send($from, $to, $subject, $content, $type = 'text/html')
     {
         $message = \Swift_Message::newInstance()
             ->setSubject($subject)
             ->setFrom($from)
             ->setTo($to)
-            ->setBody($content);
+            ->setBody($content)
+            ->setContentType($type);
         $this->get('mailer')->send($message);
     }
 
