f51ecf3d7e3b5320d584a1856303c5433033b0f9
MatthieuSimon <matthieu.simon1808@gmail.com>
2014-12-03 10:43:20 +0100

    Warn missing

--- src/Ema/RgementBundle/Controller/WarnController.php
+++ src/Ema/RgementBundle/Controller/WarnController.php
@@ -7,7 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Ema\RgementBundle\Entity\Warn;
 use Ema\RgementBundle\Entity\Retard;
+use Ema\RgementBundle\Entity\Cours;
+use Ema\RgementBundle\Entity\Etudiant;
+use Ema\RgementBundle\Entity\Participation;
+use Ema\RgementBundle\Entity\Promotion;
+use Ema\RgementBundle\Entity\Absence;
 use Ema\RgementBundle\Form\WarnType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\SimpleChoiceList;
 
 class WarnController extends Controller
 {
@@ -31,7 +37,25 @@ class WarnController extends Controller
         $form = $this->createCreateForm($entity);
         $form->handleRequest($request);
 
+		if ($form->isValid()) {
+            try {
+                $student = $this->getDoctrine()->getRepository('EmaRgementBundle:Etudiant')->find($form["nomEleve"]->getData());
 
+                $absence = new Absence();
+                $absence->setEleve($student);
+                $absence->setDateDebut($form["absenceDateDebut"]->getData());
+                $absence->setDateFin($form["absenceDateFin"]->getData());
+                $absence->setMotif($form["motif"]->getData());
+
+			    $em = $this->getDoctrine()->getManager();
+			    $em->persist($absence);
+			    $em->flush();
+                
+                return $this->render('EmaRgementBundle:Default:success.html.twig');
+            } catch (Exception $e) {
+                return $this->render('EmaRgementBundle:Default:fail.html.twig');
+            }
+		}
         return $this->render('EmaRgementBundle:Warn:absence.html.twig', array('form' => $form->createView(),));
     }
 
@@ -42,17 +66,30 @@ class WarnController extends Controller
         $form->handleRequest($request);
 
 		if ($form->isValid()) {
-           // $this->participationService->create($form->
-            
-            $delay = new Retard();
-            $delay->setIdParticipation($this->get('participation_service')->create(1,1));
-            $delay->setNomEleve($form->nomEleve);
-            $delay->setRetardHour($form->retardHour);
-            $delay->setRetardDate($form->retardDate);
+            try {
+                //$data = $form->getData();
+                $data = $form->all();
+                $cours = $this->getDoctrine()->getManager()->getRepository('EmaRgementBundle:Cours')->find(1);
+                $student = $this->getDoctrine()->getRepository('EmaRgementBundle:Etudiant')->find(1065);
+                $participation = $this->get('participation_service')->create($cours, $student);
 
-			$em = $this->getDoctrine()->getManager();
-			$em->persist($delay);
-			$em->flush();
+                $delay = new Retard();
+                $delay->setParticipation($participation);
+                $delay->setEtudiant($student);
+                $delay->setDatePrevu($form["retardDate"]->getData());
+                $time = $form["retardHour"]->getData();
+                $retardMinute = $time->format("g") * 60 + $time->format("i");
+                $delay->setDureeRetard($retardMinute);
+                $delay->setMotif($form["motif"]->getData());
+
+			    $em = $this->getDoctrine()->getManager();
+			    $em->persist($delay);
+			    $em->flush();
+                
+                return $this->render('EmaRgementBundle:Default:success.html.twig');
+            } catch (Exception $e) {
+                return $this->render('EmaRgementBundle:Default:fail.html.twig');
+            }
 		}
         return $this->render('EmaRgementBundle:Warn:delay.html.twig', array('form' => $form->createView(),));
     }
@@ -67,11 +104,17 @@ class WarnController extends Controller
     private function createCreateForm(Warn $entity)
     {
         $form = $this->createForm(new WarnType(), $entity, array(
-            'action' => $this->generateUrl('ema_rgement_warn_delay'),
+            'action' => $this->generateUrl('ema_rgement_warn_absence'),
             'method' => 'POST',
         ));
+        $i = 0;
+        $arrayEleve = array();
+        $student = new Etudiant();
 
-        //$form->add('submit', 'submit', array('label' => 'Create'));
+        foreach ($this->get('cron_controller_service')->getEtudiants() as $s) {
+            $arrayEleve[$s->getId()] = $s->getPrenom() . ' ' . $s->getNom() /*. ' - ' . $this->getDoctrine()->getRepository('EmaRgementBundle:Promotion')->find($s->getIdCybema()->getLibelle())*/;
+        }
+        $form->add('nomEleve', 'choice', array('choice_list' => new SimpleChoiceList($arrayEleve)));
 
         return $form;
     }
