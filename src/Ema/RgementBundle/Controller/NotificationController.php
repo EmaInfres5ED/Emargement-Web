ed6ec334c7a627c292d24e2909113dbec51fbca3
Yoldark34 <yoldark@gmail.com>
2014-12-03 18:59:16 +0100

    Allow to mark read all the notifications

--- src/Ema/RgementBundle/Controller/NotificationController.php
+++ src/Ema/RgementBundle/Controller/NotificationController.php
@@ -6,11 +6,13 @@ namespace Ema\RgementBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Ema\RgementBundle\Entity\Message;
 
 class NotificationController extends Controller
 {
 
     private $notificationRepository;
+    private $entityManager;
 
     public function setContainer(ContainerInterface $container = null)
     {
@@ -21,6 +23,7 @@ class NotificationController extends Controller
     private function initialize()
     {
         $this->notificationRepository = $this->getDoctrine()->getRepository("EmaRgementBundle:Notification");
+        $this->entityManager = $this->getDoctrine()->getManager();
     }
 
     public function ajaxGetNotificationsAction()
@@ -39,6 +42,35 @@ class NotificationController extends Controller
         }
 
         $response->setData($result);
+
+        return $response;
+    }
+
+    public function ajaxMarkAllTheNotificationsSawAction()
+    {
+        $response = new JsonResponse();
+
+        foreach ($this->notificationRepository->findBySaw(false) as $notification)
+        {
+            /* @var $notification Ema\RgementBundle\Entity\Notification */
+            $notification->setSaw(true);
+        }
+
+        try
+        {
+            $this->entityManager->flush();
+            $response->setData(array(
+                'type' => Message::TYPE_SUCCESS,
+                'message' => 'Mise à jour des notifications avec succès.'
+            ));
+        }
+        catch (\Exception $e)
+        {
+            $response->setData(array(
+                'type' => Message::TYPE_ERROR,
+                'message' => 'Erreur lors de la mise à jour des notifications.'
+            ));
+        }
 
         return $response;
     }
