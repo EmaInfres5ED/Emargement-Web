f3b380ff0468797843b715ec1c62182f646699ae
Zayon <pablo.godinez@zayon.eu>
2014-12-03 11:07:24 +0100

    justify service ajax function to gather unjustified absences

--- src/Ema/RgementBundle/Controller/JustifyController.php
+++ src/Ema/RgementBundle/Controller/JustifyController.php
@@ -3,18 +3,15 @@
 namespace Ema\RgementBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class JustifyController extends Controller
 {
+    private $justifyService;
+
     public function justifyListAction()
     {
-        $absences = $this->getDoctrine()
-            ->getRepository('EmaRgementBundle:Absence')
-            ->findByMotif(null);
-
-        return $this->render('EmaRgementBundle:Justify:list.html.twig', array(
-            'absences' => $absences
-        ));
+        return $this->render('EmaRgementBundle:Justify:list.html.twig');
     }
 
     public function justifyAbsenceAction()
@@ -25,5 +22,35 @@ class JustifyController extends Controller
     public function justifyDelayAction()
     {
         return $this->render('EmaRgementBundle:Justify:delay.html.twig');
+    }
+
+    public function ajaxListAbsencesToJustifyAction()
+    {
+        $this->justifyService = $this->get('justify_service');
+        $reports = $this->justifyService->getAbsencesToJustify();
+
+        $output = array(
+            'aaData' => array()
+        );
+
+        $response = new JsonResponse();
+
+        foreach ($reports as $report)
+        {
+            foreach ($report->getAbsences() as $absence)
+            {
+                $output['aaData'][] = array(
+                    'firstName' => $report->getStudent()->getPrenom(),
+                    'lastName' => $report->getStudent()->getNom(),
+                    'startDate' => $absence->getDateDebut(),
+                    'endDate' => $absence->getDateFin(),
+                    'actionUrl' => null
+                );
+            }
+        }
+
+        $response->setData($output);
+
+        return $response;
     }
 }
--- src/Ema/RgementBundle/Service/JustifyService.php
+++ src/Ema/RgementBundle/Service/JustifyService.php
@@ -4,18 +4,13 @@ namespace Ema\RgementBundle\Service;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Ema\RgementBundle\Entity\JustifyAbsence;
+use Ema\RgementBundle\Entity\Report;
 /**
  * JustifyService
  */
 class JustifyService extends Controller
 {
     private $absenceRepository;
-    private $retardRepository;
-    private $coursRepository;
-    private $participationRepository;
-    private $etudiantRepository;
-    private $presenceRepository;
 
     public function setContainer(ContainerInterface $container = null) {
         parent::setContainer($container);
@@ -24,24 +19,29 @@ class JustifyService extends Controller
 
     private function initialize() {
         $this->absenceRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Absence');
-        $this->retardRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Retard');
-        $this->coursRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Cours');
-        $this->participationRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Participation');
-        $this->etudiantRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Etudiant');
-        $this->presenceRepository = $this->getDoctrine()->getRepository('EmaRgementBundle:Presence');
     }
 
-    public function getAbsences() {
-        $absences = new JustifyAbsence();
-        $this->absenceRepository->
+    public function getAbsencesToJustify() {
+        $reports = array();
+        $studentsAbsences = array();
+        $students = array();
 
-        $absences->setNom('GODINEZ');
-        $absences->setPrenom('Pablo');
-        $absences->setCours('Management des équipes');
-        $absences->setDate(new \DateTime('now'));
-        $absences->setHeureDebut('9h00');
-        $absences->setHeureFin('12h00');
+        $absences = $this->absenceRepository->findByMotif(null);
 
-        return $absences;
+        foreach ($absences as $absence)
+        {
+            $students[$absence->getEleve()->getId()] = $absence->getEleve();
+            $studentsAbsences[$absence->getEleve()->getId()][] = $absence;
+        }
+
+        foreach ($studentsAbsences as $studentId => $absences)
+        {
+            $report = new Report();
+            $report->setAbsences($absences);
+            $report->setStudent($students[$studentId]);
+            $reports[] = $report;
+        }
+
+        return $reports;
     }
 }
